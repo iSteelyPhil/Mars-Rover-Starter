538978586b6e9ccd088c580ded40a21753ee5043
Phillip <philmography2010@gmail.com>
2024-07-20 19:22:24 -0500

    update

--- rover.js
+++ rover.js
@@ -12,25 +12,50 @@ class Rover {
       }
 
       
-     for (let i = 0; i < message.commands.length; i++) {
-         if (message.commands[i].commandType === 'STATUS_CHECK') {
-            response.results.push({
-               completed: true,
-            });
-         } else if (message.commands[i].commandType === 'MODE_CHANGE') {
-            response.results.push({
-               completed: true,
-               roverStatus: {
-                  mode: this.mode,
-                  generatorWatts: this.generatorWatts,
-                  position: this.position
-               }
-            });
-         }
-      }
-
+            for (let i = 0; i < message.commands.length; i++) {
+                        if (message.commands[i].commandType === 'STATUS_CHECK') {
+                           response.results.push({
+                              completed: true,
+                              roverStatus: {
+                                 mode: this.mode,
+                                 generatorWatts: this.generatorWatts,
+                                 position: this.position
+                              }
+                           });
+                        }
+                           if(message.commands[i].commandType === "MODE_CHANGE") {
+                              if(message.commands[i].value === "LOW_POWER") {
+                              this.mode = message.commands[i].value
+                              response.results.push({
+                              completed: false
+                           
+                           });
+                              } else if(message.commands[i].value === "NORMAL"){
+                                 this.mode = message.commands[i].value
+                                 response.results.push({
+                                 completed: true});
+                           
+                           }
+                        }
+                      if(message.commands[i].commandType === "MOVE") {
+                           if(this.mode === 'LOW_POWER'){
+                              response.results.push({
+                                 completed: false});
+                           }else{
+                           this.position = message.commands[i].value
+                           response.results.push({
+                           completed: true
+                        });
+                       
+                       
+                     }
+                  }
+                     }
+                 
+               
       return response;
-   }
-}
-
+   
+         }
+   }   
+  
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -26,7 +26,7 @@ describe("Rover class", function() {
   });
   //TEST 9
   it("response returned by receiveMessage includes two results if two commands are sent in the message", function() {
-    let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+    let commands = [new Command('MODE_CHANGE', 'NORMAL'), new Command('STATUS_CHECK')];
     let message = new Message('Test message with two commands', commands);
     let rover = new Rover(98382);    // Passes 98382 as the rover's position.
     let response = rover.receiveMessage(message);
@@ -36,13 +36,49 @@ describe("Rover class", function() {
   //TEST 10
   it("responds correctly to the status check command", function() {
     let commands = [new Command('STATUS_CHECK')];
-    let message = new Message('Test message with one commands', commands);
+    let message = new Message('Test message with one command', commands);
     let rover = new Rover(98382);    // Passes 98382 as the rover's position.
     let response = rover.receiveMessage(message);
   //  console.log(response);
   expect(response.results[0].roverStatus.mode).toEqual(rover.mode);
-  expect(response.results[1].roverStatus.generatorWatts).toEqual(rover.generatorWatts);
-  expect(response.results[2].roverStatus.position).toEqual(rover.position);
+  expect(response.results[0].roverStatus.generatorWatts).toEqual(rover.generatorWatts);
+  expect(response.results[0].roverStatus.position).toEqual(rover.position);
   });
-  
+
+
+    //TEST 11
+    it("responds correctly to the mode change command NORMAL", function() {
+      let commands = [new Command('MODE_CHANGE', 'NORMAL')];
+      let message = new Message('MODE CHANGE Normal', commands);
+      let rover = new Rover(98382);
+      let response = rover.receiveMessage(message);
+      expect(response.results[0].completed).toBe(true);
+      expect(rover.mode).toEqual('NORMAL');
+    });
+
+    //TEST 12
+    it("responds with a false completed value when attempting to move in LOW_POWER mode", function() {
+      let commands = [new Command('MODE_CHANGE', 'LOW_POWER')];
+      let message = new Message('Low Power, cannot move', commands);
+      let rover = new Rover(100);
+      let response = rover.receiveMessage(message);
+      expect(response.results[0].completed).toBe(false);
+      expect(rover.mode).toEqual('LOW_POWER');
+    });
+
+    //TEST 13
+    it("responds with the position for the move command", function() {
+      let commands = [new Command('MODE_CHANGE', 'NORMAL'), new Command('MOVE', 123)];
+      let message = new Message('MOVING', commands);
+      let rover = new Rover(123);
+      let response = rover.receiveMessage(message);
+      expect(response.results[0].completed).toBe(true);
+      expect(rover.position).toEqual(message.commands[1].value);
+
+    });
+
 });
+
+
+
+
